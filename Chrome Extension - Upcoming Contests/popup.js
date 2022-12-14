447baed902c14d644dc6520906a92056bd4a8101
Akshay Khandelwal <39028584+akshaykhandelwal0710@users.noreply.github.com>
2022-12-14 16:27:02 +0530

    styling added

--- Chrome Extension - Upcoming Contests/popup.js
+++ Chrome Extension - Upcoming Contests/popup.js
@@ -1,11 +1,46 @@
+var cnt = 1, loading = true, i;
+var par = document.getElementById("contest_details");
+
+const generate_html = function(contest){
+  ret = "<div class = \"card text-bg-primary mb-3\">";
+  ret += "<div class = \"card-header\">" + contest.head + "</div>";
+  ret += "<div class = \"card-body text-bg-info\">";
+  ret += "<div class = \"time\">" + contest.time + "</div>";
+  ret += "<div>" + contest.name + "</div>"; 
+  ret += "</div>";
+  ret += "</div>";
+  return ret;
+};
+
+//Fetch contest details
 const fetch_content = function(){
   fetch('http://127.0.0.1:5000/get_contests').then(
     response => response.text()).then(result => {
-      console.log(result);
-      var par = document.getElementById("contest_details");
-      par.innerHTML = result;
+      obj = JSON.parse(result);
+      var generatedHTML = "";
+      obj.forEach((item, index) => {
+        generatedHTML += generate_html(item);
+      });
+
+      par.innerHTML = generatedHTML;
+      loading = false;
     }
-  )
+  );
 };
+
+//Loading animation
+setInterval(() => {
+  if (loading){
+    cnt = (cnt + 1);
+    if (cnt == 4){
+      cnt = 1;
+    }
+
+    par.innerHTML = "Loading ";
+    for (i = 0; i < cnt; i++){
+      par.innerHTML += ".";
+    };
+  };
+}, 300);
 
 fetch_content();
--- Web Scraper/scraper.py
+++ Web Scraper/scraper.py
@@ -10,17 +10,21 @@ cfTZObject = datetime.timezone(cfTimeDelta, name = "CF")
 istTimeDelta = datetime.timedelta(hours = 5, minutes = 30)
 istTZObject = datetime.timezone(istTimeDelta, name = "IST")
 
-def print_contest(contest_name, datetime_obj, platform):
-  res = "\n"
+def contest_to_json(contest_name, datetime_obj, platform):
+  res = "{"
   contest_start = datetime_obj.astimezone(istTZObject).strftime("%d %b, %Y %I:%M %p")
 
-  res += "NEXT " + platform.upper() + " CONTEST:\n"
-  res += contest_start + '\t' + contest_name + "\n"
-  res += "\n"
+  res += "\"head\": "
+  res += "\"NEXT " + platform.upper() + " CONTEST\""
+  res += ", \"time\": "
+  res += "\"" + contest_start + "\""
+  res += ", \"name\": "
+  res += "\"" + contest_name + "\""
+  res += "}"
   return res
 
 def get_contests():
-  result = ""
+  result = "["
 
   # Codeforces
   URL = "https://codeforces.com/contests"
@@ -36,7 +40,7 @@ def get_contests():
   contest_name = fields[0].string.strip()
   contest_start = next_contest.find(class_ = "format-time").string.strip()
   datetime_obj = datetime.datetime.strptime(contest_start, "%b/%d/%Y %H:%M").replace(tzinfo = cfTZObject)
-  result += print_contest(contest_name, datetime_obj, "codeforces")
+  result += contest_to_json(contest_name, datetime_obj, "codeforces")
 
   #Atcoder
   URL = "https://atcoder.jp/contests/"
@@ -50,7 +54,9 @@ def get_contests():
   a_tags = next_contest.find_all("a")
   contest_name = a_tags[1].string
   datetime_obj = datetime.datetime.strptime(a_tags[0].string, "%Y-%m-%d %H:%M:%S%z")
-  result += print_contest(contest_name, datetime_obj, "atcoder")
+  result += ", "
+  result += contest_to_json(contest_name, datetime_obj, "atcoder")
 
+  result += "]"
   print(result)
   return result
